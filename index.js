45b07064827f1d9bb34be20f2a2d89c2d252eb50
idham <idham290502@gmail.com>
2023-04-07 09:30:05 +0800

    login and register api

--- index.js
+++ index.js
@@ -2,12 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let dbUsers = [ {
+    username: "ali" ,
+    password: "password",
+    name:"ali",
+    email: "ali@utem.com"
+
+
+},
+{
+    username: "johnny",
+    password: "yespapa",
+    name: "johnny johnny",
+    email: "eatingsugar@nopapa.com"
+}]
+
+//enable json body parsing
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  let data = req.body
+  res.send(login(data.username,data.password))
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/bye',(req,res) => {
-    res.send('bye bye world')
+app.post('/',(req,res) => {
+  let data = req.body
+  res.send('post request'+ JSON.stringify(data))  
 })
 
 app.listen(port, () => {
